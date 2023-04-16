175797cc3327b28e53ee5199d9db238f38185033
Samuel Ebuka <samuel.ebuka@gmail.com>
2023-04-16 13:21:02 +0200

    background imagen

---
+++ components/content/Background.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="relative">
+		<img v-if="imagen" :src="imagen" alt="fondo" class="relative top-0 object-cover w-full h-screen" />
+
+		<div class="absolute top-0 flex flex-col justify-center w-full h-screen not-prose">
+          <slot></slot>
+      </div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	imagen: {
+		type: String,
+		required: false,
+	},
+});
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/content/BackgroundImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="h-screen bg-center bg-cover"
+    :style="`background-image: url(${src})`"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "BackgroundImage",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
