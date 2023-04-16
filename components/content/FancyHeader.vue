e3a78a76d827f318cdeaa0e915dafaccd712c6b5
Samuel Ebuka <samuel.ebuka@gmail.com>
2023-04-16 10:13:05 +0200

    Synced by Front Matter

--- components/content/FancyHeader.vue
+++ components/content/FancyHeader.vue
@@ -7,20 +7,20 @@
     <div class="text-center hero-content text-neutral-content">
     
       <div class="py-20 max-w-7xl">
-        <h1 class="py-2 text-3xl font-bold md:text-7xl ">
+        <h1 class="py-2 text-4xl font-bold md:text-7xl ">
           <ContentSlot :use="$slots.titulo" unwrap="p" />
         </h1>
 
         <h2 class="p-2 text-3xl lg:text-4xl md:pb-10">
           <ContentSlot :use="$slots.subtitulo" unwrap="p" />
         </h2>
-        <p class="w-full max-w-5xl mb-10 text-2xl text-justify md:mb-24">
+        <p class="hidden w-full max-w-5xl mb-10 text-2xl text-justify md:block md:mb-24">
           <ContentSlot :use="$slots.texto" unwrap="p" />
         </p>
         <!-- <button v-if="$slots.boton" class="btn btn-primary">
           <ContentSlot :use="$slots.boton" unwrap="p" />
         </button> -->
-        <div class="flex justify-around w-full">
+        <div class="justify-around hidden w-full md:flex">
           <slot name="accion" />
         </div>
       </div>
