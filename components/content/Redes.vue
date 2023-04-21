e1800bf70fd41058bcdc996f2f08aa4c219f85d7
Samuel Ebuka <samuel.ebuka@gmail.com>
2023-04-21 13:10:01 +0200

    Logo Telegram

--- components/content/Redes.vue
+++ components/content/Redes.vue
@@ -4,6 +4,7 @@
 				<a class="text-xl btn btn-ghost" href="https://www.youtube.com/channel/UCYinQCLEznaHNZFzxbw-dMw"><Icon name="ph:youtube-logo" /></a> 
 				<a class="text-xl btn btn-ghost" href="mailto:mklmusic6@gmail.com"><Icon name="ph:envelope" /></a> 
 				<a class="text-xl btn btn-ghost" href="https://wa.me/630830818"><Icon name="ph:whatsapp-logo" /></a> 
+				<a class="text-xl btn btn-ghost" href="https://t.me/+SCjlxQX1VAM5OTNk"><Icon name="ph:telegram-logo" /></a> 
 				<a class="text-xl btn btn-ghost" href="tel:+630830818"><Icon name="ph:phone" /></a> 
 			</div>
 </template>
